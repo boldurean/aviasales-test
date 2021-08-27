bcb77177dcbe317ff41d1bf644848c12dda1b4c0
vsbdn <v.boldurean@gmail.com>
2021-08-27 16:40:24 +0100

    add memo for rendered components

--- src/features/filter/Filters.js
+++ src/features/filter/Filters.js
@@ -23,75 +23,77 @@ const filtersData = {
   three: {maxStops: 3, text: '3 пересадки', isChecked: false},
 };
 
-const Filters = ({ setFilteredTickets, initialTickets }) => {
-  const [filters, setFilters] = useState(filtersData);
-  const { isNetworkError } = useData();
+const Filters = React.memo(
+  ({ setFilteredTickets, initialTickets }) => {
+    const [filters, setFilters] = useState(filtersData);
+    const { isNetworkError } = useData();
 
-  const disabled = isNetworkError;
+    const disabled = isNetworkError;
 
-  const handleUpdateFilters = (name) => () => {
-    const newFilters = {...filters};
-    const names = Object.keys(filters).filter(n => n !== 'all');
+    const handleUpdateFilters = (name) => () => {
+      const newFilters = {...filters};
+      const names = Object.keys(filters).filter(n => n !== 'all');
 
-    switch (name) {
-      case 'all':
-        const { isChecked } = newFilters[name];
-        if (isChecked) {
-          newFilters[name].isChecked = !isChecked
-          names.forEach((n) => newFilters[n].isChecked = true);
-        } else {
-        newFilters[name].isChecked = !isChecked
-        names.forEach(n => newFilters[n].isChecked = false);
-        }
-        break;
-      default:
-        newFilters[name].isChecked = !newFilters[name].isChecked
-        const active = Object.values(newFilters).filter((f) => f.isChecked);
-        active.length === 0 ? newFilters.all.isChecked = true : newFilters.all.isChecked = false;
-    }
-    setFilters(newFilters);
-  }
-
-  useEffect(() => {
-    if (!initialTickets) {
-      setFilteredTickets(null)
-      return
-    };
-
-    const activeFilters = Object.values(filters)
-      .filter((f) => f.isChecked)
-      .map((f) => f.maxStops);
-
-    const maxStops = Math.max(...activeFilters)
-    const newTicketsList = initialTickets.filter((ticket) => {
-      const stops = ticket.segments.reduce((acc, direction) => acc + direction.stops.length, 0);
-      return stops <= maxStops;
-    })
-    setFilteredTickets(newTicketsList);
-  }, [filters, initialTickets, setFilteredTickets])
-
-  return (
-    <div className="filters__container">
-      <p className="filters__title">Количество пересадок</p>
-      <form className="filters__form">
-        <ul className="filters__list">
-          {Object.entries(filtersData).map(([name, { maxStops, text,  isChecked }]) => {
-            return (
-              <ListItem
-                key={maxStops}
-                name={name}
-                text={text}
-                handleToggle={handleUpdateFilters(name)}
-                checked={isChecked}
-                disabled={disabled}
-              />
-            );
-          })
+      switch (name) {
+        case 'all':
+          const { isChecked } = newFilters[name];
+          if (isChecked) {
+            newFilters[name].isChecked = !isChecked
+            names.forEach((n) => newFilters[n].isChecked = true);
+          } else {
+            newFilters[name].isChecked = !isChecked
+            names.forEach(n => newFilters[n].isChecked = false);
           }
-        </ul>
-      </form>
-    </div>
-  );
-};
+          break;
+        default:
+          newFilters[name].isChecked = !newFilters[name].isChecked
+          const active = Object.values(newFilters).filter((f) => f.isChecked);
+          active.length === 0 ? newFilters.all.isChecked = true : newFilters.all.isChecked = false;
+      }
+      setFilters(newFilters);
+    }
+
+    useEffect(() => {
+      if (!initialTickets) {
+        setFilteredTickets(null)
+        return
+      };
+
+      const activeFilters = Object.values(filters)
+        .filter((f) => f.isChecked)
+        .map((f) => f.maxStops);
+
+      const maxStops = Math.max(...activeFilters)
+      const newTicketsList = initialTickets.filter((ticket) => {
+        const stops = ticket.segments.reduce((acc, direction) => acc + direction.stops.length, 0);
+        return stops <= maxStops;
+      })
+      setFilteredTickets(newTicketsList);
+    }, [filters, initialTickets, setFilteredTickets])
+
+    return (
+      <div className="filters__container">
+        <p className="filters__title">Количество пересадок</p>
+        <form className="filters__form">
+          <ul className="filters__list">
+            {Object.entries(filtersData).map(([name, { maxStops, text,  isChecked }]) => {
+              return (
+                <ListItem
+                  key={maxStops}
+                  name={name}
+                  text={text}
+                  handleToggle={handleUpdateFilters(name)}
+                  checked={isChecked}
+                  disabled={disabled}
+                />
+              );
+            })
+            }
+          </ul>
+        </form>
+      </div>
+    );
+  }
+);
 
 export default Filters;
--- src/features/sortingButtons/sortingButtons.js
+++ src/features/sortingButtons/sortingButtons.js
@@ -1,6 +1,6 @@
 import orderBy from 'lodash/orderBy.js';
 import './sortingButtons.scss';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 
 const Button = ({ text, toggle, active, disabled }) => {
   return <button onClick={toggle} data-active={active} disabled={disabled} >{text}</button>
@@ -13,58 +13,61 @@ const buttons = [
   {type: 'optimal', text: "Оптимальный"},
 ];
 
-const SortingButtons = ({ filteredTickets, setTickets }) => {
-  const [currentActive, setCurrentActive] = useState(null);
+const SortingButtons = React.memo(
+  ({ filteredTickets, setTickets }) => {
+    const [currentActive, setCurrentActive] = useState('optimal');
 
-  const handleSortGenerator = (type) => {
-    switch (type) {
-      case 'price':
-        return () => {
-          const sorted = orderBy(filteredTickets, 'price');
-          setTickets(sorted);
-          setCurrentActive(type);
-        }
-      case 'duration':
-        return () => {
-          const sorted = orderBy(filteredTickets, (ticket) => {
-            return ticket.segments.reduce((acc, s) => acc + s.duration, 0)
-          }, ['asc']);
-          setTickets(sorted);
-          setCurrentActive(type);
-        };
-      default:
-        return () => {
-          const sorted = orderBy(filteredTickets, (ticket) => {
-            return ticket.price + ticket.segments.reduce((acc, s) => acc + s.duration, 0)
-          }, ['asc']);
-          setTickets(sorted);
-          setCurrentActive(type);
-        };
-    }
-  };
-
-  useEffect(() => {
-    const initialSort = () => {
-      const sorted = orderBy(filteredTickets, (ticket) => {
-        return ticket.price + ticket.segments.reduce((acc, s) => acc + s.duration, 0)
-      }, ['asc']);
-      setTickets(sorted);
+    const handleSortGenerator = (type) => {
+      switch (type) {
+        case 'price':
+          return () => {
+            const sorted = orderBy(filteredTickets, 'price');
+            setTickets(sorted);
+            setCurrentActive(type);
+          }
+        case 'duration':
+          return () => {
+            const sorted = orderBy(filteredTickets, (ticket) => {
+              return ticket.segments.reduce((acc, s) => acc + s.duration, 0)
+            }, ['asc']);
+            setTickets(sorted);
+            setCurrentActive(type);
+          };
+        default:
+          return () => {
+            const sorted = orderBy(filteredTickets, (ticket) => {
+              return ticket.price + ticket.segments.reduce((acc, s) => acc + s.duration, 0)
+            }, ['asc']);
+            setTickets(sorted);
+            setCurrentActive(type);
+          };
+      }
     };
-    initialSort();
-  }, [filteredTickets, setTickets])
 
-  return (
-    <div className="sorting-buttons-wrapper">
-      {buttons.map(({ type, text }) =>
-        <Button
-          key={type}
-          text={text}
-          toggle={handleSortGenerator(type)}
-          active={currentActive === type}
-          disabled={!filteredTickets.length}
-        />)}
-    </div>
-  );
-}
+    useEffect(() => {
+      const initialSort = () => {
+        const sorted = orderBy(filteredTickets, (ticket) => {
+          return ticket.price + ticket.segments.reduce((acc, s) => acc + s.duration, 0)
+        }, ['asc']);
+        setTickets(sorted);
+      };
+      initialSort();
+    }, [filteredTickets, setTickets])
+
+
+    return (
+      <div className="sorting-buttons-wrapper">
+        {buttons.map(({ type, text }) =>
+          <Button
+            key={type}
+            text={text}
+            toggle={handleSortGenerator(type)}
+            active={currentActive === type}
+            disabled={!filteredTickets.length}
+          />)}
+      </div>
+    );
+  }
+)
 
 export default SortingButtons;
